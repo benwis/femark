aed826383505fbf62c9d32355162e192f596ca02
Ben Wishoshavich <ben@celcyon.com>
2022-06-16 12:50:33 -0700

    Need a proper tag

--- __test__/index.spec.mjs
+++ __test__/index.spec.mjs
@@ -3,6 +3,7 @@ import test from 'ava'
 import { renderMarkdown } from '../index.js'
 
 // Can it render a basic Hello World string?
+// TODO: Add more tests
 test('render Hello, World!', (t) => {
   t.is(renderMarkdown("# Hello, World!"), `<h1>Hello, World!</h1>\n`)
 })
